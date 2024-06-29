e236ffa5f567c27e89485343a0c1a92e96a8f7dc
Azamat Hasanov <politicalhe03@gmail.com>
2024-06-29 09:49:04 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function masinHesabi() {
+    debugger
+    let selectedcar, i
+    for (i = 0; document.querySelector('#carTipi').length; i++) {
+        if (document.querySelector('#carTipi').options[i].selected) {
+            selectedcar = document.querySelector('#carTipi').options[i].value
+        }
+        console.log(selectedcar);
+    }
+}
